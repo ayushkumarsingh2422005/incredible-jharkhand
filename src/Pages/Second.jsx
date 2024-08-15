3123bf7031429969a0f69c1963de0c42e3d7687b
ayush kumar <ayush2422005@gmail.com>
2024-08-15 22:36:14 +0530

    updates

--- src/Pages/Second.jsx
+++ src/Pages/Second.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function Second() {
   return (
     <div className='w-full'>
-      <div className='text-center md:text-7xl text-4xl font-bold'>Jharkhand</div>
+      <div className='text-center md:text-7xl text-4xl font-bold'>About Us</div>
       <div className='md:w-[60%] w-[80%] mx-auto flex my-10 gap-10 md:flex-row flex-col md:text-xl text-base'>
-        <div className='w-full text-center'>India's vibrant state of natural wonders and industrial prowess. From the steel city of Jamshedpur to the sacred temples of Deoghar, Jharkhand blends tradition with progress. Its lush forests, waterfalls, and wildlife parks captivate nature lovers, while the state's rich tribal culture and festivals reflect its deep heritage. Jharkhand's role as an industrial powerhouse fuels India’s growth, making it a land where history, culture, and innovation converge, ready to shine on the global stage.</div>
+        <div className='w-full text-center'>Welcome to Incredible Jharkhand! We're dedicated for sharing the vibrant culture, rich traditions, and stunning landscapes of Jharkhand. Through engaging content and dynamic connections on social media, we bring the essence of Jharkhand to life. Join us as we connect with influencers, locals, and enthusiasts to highlight the heart and soul of this remarkable state. Discover, engage, and be a part of Jharkhand’s incredible journey with us!</div>
         {/* <div className='w-full text-justify'>while the state's rich tribal culture and festivals reflect its deep heritage. Jharkhand's role as an industrial powerhouse fuels India’s growth, making it a land where history, culture, and innovation converge, ready to shine on the global stage.</div> */}
       </div>
     </div>
