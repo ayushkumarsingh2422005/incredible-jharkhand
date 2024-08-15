708120f22e2630f0565adf133cb8fa7af981c767
ayush kumar <ayush2422005@gmail.com>
2024-08-15 22:27:07 +0530

    updates

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -9,7 +9,7 @@ export default function Footer() {
         <div className='w-full p-5 bg-gray-900 text-white flex justify-between'>
             <div>
                 <div>&copy; Incredible Jharkhand 2024-25</div>
-                <div>Designed and developed <br className='md:hidden block'/> by a AlgoByte</div>
+                <div>Designed and developed <br className='md:hidden block'/> by AlgoByte</div>
             </div>
             <div className='flex gap-3 items-center text-2xl pr-5 flex-col md:flex-row justify-center'>
                 <a href='https://www.instagram.com/ayush_the_dev' target='_blank'><BsInstagram/></a>
